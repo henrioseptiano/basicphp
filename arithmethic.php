93918b17a4bc44eec39bbbd78cf28812e3a1800a
henrioseptiano <rioseptiano123@gmail.com>
2015-12-02 17:04:25 +0700

    Added Arithmethic operators tutorial

---
+++ arithmethic.php
@@ -0,0 +1,19 @@
+<?php 
+
+      //adding numbers and assign to a variable name add
+      $add = 5 + 10; 
+
+      //echo the variable and put the next line 
+      echo $add.'\n';  // remember \n means you can print another variable or anything to the next line of your output
+      
+      //substract numbers and assign to a variable name sub
+      $sub = 10 - 2;
+
+      echo $sub.'\n';
+
+      /* just remember whatever the arithmethic operators you want to put it
+          you have two options to do it
+
+	  $sub = <number1> <arithmethic operators> <number2>;
+	  or
+	  $number1 <arithmethic operators>= <number2>;
